8171dc59e5d80e2bc2f56e17626ac71fa1be7850
Hamza <hamzaelaissaouibxl@gmail.com>
2025-02-05 16:37:48 +0100

    ajout des icons

--- src/Components/MiniCard/MiniCard.jsx
+++ src/Components/MiniCard/MiniCard.jsx
@@ -1,5 +1,5 @@
 import TroisP from "../../../../images/icon-ellipsis.svg";
-// import Ex from "../../../../images/icon-exercise.svg";
+import Ex from "../../../../images/icon-exercise.svg";
 // import Play from "../../../../images/icon-play.svg";
 // import Self from "../../../../images/icon-self-care.svg";
 // import Social from "../../../../images/icon-social.svg";
@@ -11,7 +11,8 @@ function MiniCard() {
   return (
     <>
       <div>
-        <div className="bg-conic-{Self} rounded-3xl h-100 rounded-3xl">
+        <div className="rounded-3xl h-100 rounded-3xl">
+            <img src={Ex} alt="" />
             <div className=" bg-[#1D204B] rounded-3xl text-[#FAFDFF] h-80 w-90">
                 <div className="flex justify-between items-center pt-5 pl-5 pr-5">
                     <h1 className="text-2xl m-5  font-bold">Work</h1>
